45b24ffa90246321a243ad9259201a99db1e12f1
Ederson Macedo <ederson.mos@gmail.com>
2022-12-29 14:05:16 -0300

    Created example of var about output before set value
    In that example expect output undefined and reason of not use var

---
+++ example-var-2.js
@@ -0,0 +1,6 @@
+console.log(pi);
+// expect output: undefined
+// no generated error because JS create variable in init of execute, showing undefined. 
+// But the correct would be to genarete an error. Therefore must use 'const' or 'let'
+
+var pi = 3.14159265359;
