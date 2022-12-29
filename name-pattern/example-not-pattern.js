dbd1abb09de8bfad8bedf3257b2aef40529b0c69
Ederson Macedo <ederson.mos@gmail.com>
2022-12-29 17:10:17 -0300

    Create example pattern name of variables

---
+++ name-pattern/example-not-pattern.js
@@ -0,0 +1,4 @@
+let 123name;
+let @name;
+let "name;
+let 'name;
---
+++ name-pattern/example-pattern.js
@@ -0,0 +1,6 @@
+let name123;
+let Name123;
+let $name123;
+let _name123;
+// A valid identifier must start with 
+// [a-zA-Z_$] followed by [a-zA-Z0-9_$]
