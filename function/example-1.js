d3664c72d3006b23431672594af8aac59235b9da
Ederson Macedo <ederson.mos@gmail.com>
2022-12-29 17:00:39 -0300

    Create example of never declare without var, let or const

---
+++ function/example-1.js
@@ -0,0 +1,7 @@
+(function () {
+    pi = 3.14159265359;
+})();
+console.log(pi);
+
+// Never declare variable without var, let and const. 
+// Because the declaration without, go global. May generate error
